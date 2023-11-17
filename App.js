7f517caed359c5e6f7cfdc0a479efc41466bf0bd
ZeRav3n <ze.rav3n@gmail.com>
2023-11-17 13:43:04 -0500

    App.js updated

--- App.js
+++ App.js
@@ -1,11 +1,71 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import Onboarding from './screens/Onboarding';
+import React, {useState, useEffect, useCallback} from 'react';
+import {Platform, StatusBar, Image} from 'react-native';
+import {Asset} from 'expo-asset';
+import {Block, GalioProvider} from 'galio-framework';
+import {NavigationContainer} from '@react-navigation/native';
+import * as SpalshScreen from "expo-splash-screen";
+import {Images, products, materialTheme} from './constants';
+import Screen from './navigation/Screens';
+import {enableScreens} from 'react-native-screens';
 
-const App = () => {
-  return (
-    <Onboarding />
-  );
+enableScreens();
+
+const assetImages = [
+  Images.Pro,
+  Images.Profile,
+  Images.Avatar,
+  Images.Onboarding,
+];
+
+products.map((product) => assetImages.push(product.image));
+
+function cacheImages(images) {
+  return images.map((image) => {
+    if (typeof image === 'string') {
+      return Image.prefetch(image);
+    }
+    return Asset.fromModule(image).downloadAsync();
+  });
 }
 
-export default App;
+export default function App() {
+  const [appIsReady, setAppIsReady] = useState(false);
+
+  useEffect(() => {
+    async function prepare(){
+      try{
+        await _loadResourcesAsync();
+      } catch(e){
+        console.warn(e);
+      } finally{
+        setAppIsReady(true);
+      }
+    }
+    prepare();
+  }, []);
+
+  const _loadResourcesAsync = async () => {
+    return Promise.all([...cacheImages(assetImages)]);
+  }
+
+  const onLayoutRootView = useCallback(async () => {
+    if(appIsReady){
+      await SpalshScreen.hideAsync();
+    }
+  }, [appIsReady]);
+
+  if(!appIsReady){
+    return null;
+  }
+
+  return (
+    <NavigationContainer onReady={onLayoutRootView}>
+      <GalioProvider theme={materialTheme}>
+        <Block flex onLayout={onLayoutRootView}>
+          {Platform.OS === 'ios' && <StatusBar barStyle='default' />}
+          <Screen />
+        </Block>
+      </GalioProvider>
+    </NavigationContainer>
+  );
+}
